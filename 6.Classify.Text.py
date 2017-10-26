b70832f3b017a386218dcd5f3aa57d597eef3bfa
Minzhe Zhang <zenroute.mzhang@gmail.com>
2017-10-26 13:52:56 -0500

    6.classify text

---
+++ 6.Classify.Text.py
@@ -0,0 +1,61 @@
+### Gender Identification
+import nltk
+from nltk.corpus import names
+import random
+def gender_features(word):
+    return {'last_letter': word[-1]}
+names = ([(name, 'male') for name in names.words('male.txt')] + [(name, 'female') for name in names.words('female.txt')])
+random.shuffle(names)
+
+featuresets = [(gender_features(n), g) for (n,g) in names]
+train_set, test_set = featuresets[500:], featuresets[:500]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+classifier.classify(gender_features('Neo'))
+classifier.classify(gender_features('Trinity'))
+nltk.classify.accuracy(classifier, test_set)
+classifier.show_most_informative_features(5)
+
+from nltk.classify import apply_features
+train_set = apply_features(gender_features, names[500:])
+test_set = apply_features(gender_features, names[:500])
+
+### Choosing the Right Features
+def gender_features2(name):
+    features = {}
+    features["firstletter"] = name[0].lower()
+    features["lastletter"] = name[-1].lower()
+    for letter in 'abcdefghijklmnopqrstuvwxyz':
+        features["count(%s)" % letter] = name.lower().count(letter)
+        features["has(%s)" % letter] = (letter in name.lower())
+    return features
+gender_features2('John')
+
+featuresets = [(gender_features2(n), g) for (n,g) in names]
+train_set, test_set = featuresets[500:], featuresets[:500]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+print(nltk.classify.accuracy(classifier, test_set))
+
+### Document Classification
+from nltk.corpus import movie_reviews
+documents = [(list(movie_reviews.words(fileid)), category)
+             for category in movie_reviews.categories()
+             for fileid in movie_reviews.fileids(category)]
+random.shuffle(documents)
+
+all_words = nltk.FreqDist(w.lower() for w in movie_reviews.words())
+word_features = list(all_words.keys())[:2000]
+def document_features(document):
+    document_words = set(document)
+    features = {}
+    for word in word_features:
+        features['contains(%s)' % word] = (word in document_words)
+    return features
+document_features(movie_reviews.words('pos/cv957_8737.txt'))
+
+featuresets = [(document_features(d), c) for (d,c) in documents]
+train_set, test_set = featuresets[100:], featuresets[:100]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+nltk.classify.accuracy(classifier, test_set)
+classifier.show_most_informative_features(5)
